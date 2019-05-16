cf1aea5755a0a7f2314c9b38e642ec3bf5654365
Brad Hoffman <bradleydavidhoffman@gmail.com>
2019-05-16 16:50:43 -0400

    initial commit of message code. Gathered and modified from the tutorial on codelabs.developers.google.com

---
+++ homies/lib/screens/home.dart
@@ -0,0 +1,73 @@
+import "package:flutter/material.dart";
+
+import "../items/message.dart";
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final TextEditingController _textEditingController = new TextEditingController();
+  final List<ChatMessage> _messages = <ChatMessage>[];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("HOMIES"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            child: ListView.builder(
+              padding: EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, int index) => _messages[index],
+              itemCount: _messages.length,
+            )
+          ),
+          new Divider(height: 1.0),
+          Container(
+            decoration: BoxDecoration(color: Theme.of(context).cardColor),
+            child: _buildTextComposer(),
+          )
+        ],
+      ),
+    );
+  }
+
+  _handleSubmitted(String text) {
+    _textEditingController.clear();
+    ChatMessage message = new ChatMessage(text: text);
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+
+  Widget _buildTextComposer() {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: TextField(
+                controller: _textEditingController,
+                decoration: InputDecoration.collapsed(hintText: "Message"),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                icon: Icon(Icons.send),
+                onPressed: () => _handleSubmitted(_textEditingController.text),
+              ),
+            )
+          ],
+        )
+      ),
+    );
+  }
+}
