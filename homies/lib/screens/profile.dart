e48dd73229bbcc09e0bac9b1403ec35cdbf27200
Brad Hoffman <bradleydavidhoffman@gmail.com>
2019-07-02 16:21:48 -0400

    displaying a default profile image for the user

--- homies/lib/screens/profile.dart
+++ homies/lib/screens/profile.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import "package:flutter/material.dart";
+import "package:homies/services/profile.dart" as profileService;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ProfilePage extends StatefulWidget {
   @override
@@ -6,6 +10,14 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  SharedPreferences prefs;
+  File image;
+
+  _getPrefs() async {
+    prefs = await SharedPreferences.getInstance();
+    return true;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,16 +32,65 @@ class _ProfilePageState extends State<ProfilePage> {
           )
         ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Text("TEST")
-        ],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: 200,
+                height: 200,
+                padding: EdgeInsets.all(2.0),
+                decoration: new BoxDecoration(
+                  color: Colors.grey,
+                  shape: BoxShape.circle
+                ),
+                child: FutureBuilder(
+                  future: _getImage(),
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    switch (snapshot.connectionState) {
+                      case ConnectionState.none:
+                        // TODO: Handle this case.
+                        return CircularProgressIndicator();
+                        break;
+                      case ConnectionState.waiting:
+                        // TODO: Handle this case.
+                        return CircularProgressIndicator();
+                        break;
+                      case ConnectionState.active:
+                        // TODO: Handle this case.
+                        return CircularProgressIndicator();
+                        break;
+                      case ConnectionState.done:
+                        // TODO: Handle this case.
+                        if (snapshot.hasData) {
+                          return snapshot.data;
+                        }
+                        break;
+                    }
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
 
-  getImage() {
+  _getImage() async {
+    prefs = await SharedPreferences.getInstance();
 
+    if (prefs.getString("IMAGE_PROFILE_PATH") != null) {
+      return CircleAvatar(
+        backgroundImage: FileImage(File(prefs.getString("IMAGE_PROFILE_PATH")))
+      );
+    } else {
+      return CircleAvatar(
+        backgroundImage: ExactAssetImage("assets/profile.png"),
+        backgroundColor: Colors.white,
+      );
+    }
   }
 }
--- homies/lib/services/profile.dart
+++ homies/lib/services/profile.dart
@@ -1,8 +1,23 @@
+import 'dart:io';
 
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ProfileService {
+  SharedPreferences prefs;
 
-  getImage() {
+  storeImageFromGallery() async {
+    final File image = await ImagePicker.pickImage(source: ImageSource.camera);
+    Directory appDocDir = await getApplicationDocumentsDirectory();
+    final String path = appDocDir.path;
+    final File profileImage = await image.copy('$path/profile.image');
+
+    prefs = await SharedPreferences.getInstance();
+    prefs.setString("PROFILE_IMAGE_PATH", profileImage.path);
+  }
+
+  storeImageFromNetwork() async {
 
   }
 }
