22470c2afae3ae17de18a0dc2e8e386946996bc8
Brad Hoffman <bradleydavidhoffman@gmail.com>
2019-07-02 15:05:25 -0400

    uploaded profile images works

--- Server/routes/profile.js
+++ Server/routes/profile.js
@@ -2,8 +2,39 @@ const passport = require('passport');
 require('../config/passport')(passport);
 const router = require('express').Router();
 const jwt = require('jwt-simple');
+const multer = require('multer');
+const fs = require('fs');
 
 const User = require('../models/user.js');
+
+var storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    console.log(req.body);
+    if (!fs.existsSync('./images')) {
+      console.log("Creating images folder.");
+      fs.mkdirSync('./images');
+    }
+
+    var dir = "./images/" + req.user.id;
+    if (!fs.existsSync(dir)) {
+      console.log("Creating dir:" + dir);
+      fs.mkdirSync(dir);
+    }
+
+    cb(null, dir);
+  },
+  filename: function(req, file, cb) {
+    cb(null, "profile.jpg");
+  }
+});
+
+var upload = multer({
+  storage: storage,
+  onError: function(err, next) {
+    console.log(err);
+    next(err);
+  }
+});
 
 router.get('/', passport.authenticate('jwt', { session: false }), function(req, res) {
   console.log(req.user);
@@ -37,8 +68,11 @@ router.post('/', passport.authenticate("jwt", {session: false}), function(req, r
         }
       });
     }
-  })
+  });
+});
 
-})
+router.post('/image', passport.authenticate('jwt', {session: false}), upload.single('image'), function(req, res) {
+  res.status(200).json({success: true, msg: "Image uploaded."});
+});
 
 module.exports = router;
