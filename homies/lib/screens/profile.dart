045458339ffa44b99d2439814969148392ea23a4
Brad Hoffman <bradleydavidhoffman@gmail.com>
2019-07-08 09:50:35 -0400

    moved logout function to the profile screen

--- homies/lib/screens/profile.dart
+++ homies/lib/screens/profile.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import "package:flutter/material.dart";
 import "package:homies/services/profile.dart";
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 
 class ProfilePage extends StatefulWidget {
   @override
@@ -15,6 +16,8 @@ class _ProfilePageState extends State<ProfilePage> {
   SharedPreferences prefs;
   File image;
   bool isEditing = false;
+
+  final client = http.Client();
 
   @override
   void initState() {
@@ -102,11 +105,63 @@ class _ProfilePageState extends State<ProfilePage> {
                 controller: birthNameController,
                 onChanged: (value) => profileService.displayName = value,
               ),
-            ],
+              Container(
+                child: MaterialButton(
+                  color: Colors.red,
+                  child: Text("Logout", style: TextStyle(color: Colors.white),),
+                  onPressed: () {
+                    _logout();
+                  },
+                ),
+              )
+            ]
           ),
         ),
       ),
     );
+  }
+
+  _logout() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    var token = prefs.getString("TOKEN");
+    
+    var headers = {
+      "content-type": "application/json",
+      "accept": "application/json",
+      "Authorization": token
+    };
+
+    var url = "http://127.0.0.1:3000/auth/signout";
+    var response = await this.client.get(url, headers: headers);
+
+    if (response.statusCode != 200) {
+      print(response.body);
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Cannot communicate with servers at this instance."),
+            //content: Center(child: Text("Please try again.")),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("DONE"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        }
+      );
+    } else {
+      prefs.setBool("LOGGED_IN", false);
+
+      Navigator.of(context).pop();
+      Navigator.of(context).pushReplacementNamed("/");
+    }
+
+    print(response.body);
   }
 
   _getImage() async {
--- homies/lib/screens/settings.dart
+++ homies/lib/screens/settings.dart
@@ -1,6 +1,4 @@
 import "package:flutter/material.dart";
-import "package:http/http.dart" as http;
-import "package:shared_preferences/shared_preferences.dart";
 
 class SettingsPage extends StatefulWidget {
   @override
@@ -8,7 +6,6 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
-  final client = http.Client();
 
   @override
   Widget build(BuildContext context) {
@@ -22,13 +19,8 @@ class _SettingsPageState extends State<SettingsPage> {
           child: Column(
             children: <Widget>[
               Container(
-                child: MaterialButton(
-                  color: Colors.red,
-                  child: Text("Logout", style: TextStyle(color: Colors.white),),
-                  onPressed: () {
-                    _logout();
-                  },
-                ),
+                width: 0,
+                height: 0,
               )]
           ),
         ),
@@ -36,46 +28,4 @@ class _SettingsPageState extends State<SettingsPage> {
     );
   }
 
-  _logout() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-
-    var token = prefs.getString("TOKEN");
-    
-    var headers = {
-      "content-type": "application/json",
-      "accept": "application/json",
-      "Authorization": token
-    };
-
-    var url = "http://127.0.0.1:3000/auth/signout";
-    var response = await this.client.get(url, headers: headers);
-
-    if (response.statusCode != 200) {
-      print(response.body);
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text("Cannot communicate with servers at this instance."),
-            //content: Center(child: Text("Please try again.")),
-            actions: <Widget>[
-              FlatButton(
-                child: Text("DONE"),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              )
-            ],
-          );
-        }
-      );
-    } else {
-      prefs.setBool("LOGGED_IN", false);
-
-      Navigator.of(context).pop();
-      Navigator.of(context).pushReplacementNamed("/");
-    }
-
-    print(response.body);
-  }
 }
