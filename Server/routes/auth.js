362e65fc1c30735c0544768203de12cd4138913a
Brad Hoffman <bradleydavidhoffman@gmail.com>
2019-07-16 14:01:02 -0400

    added image upload as part of the registration process

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -4,31 +4,73 @@ const config = require('../config/database.js');
 require('../config/passport')(passport);
 const router = require('express').Router();
 const jwt = require('jwt-simple');
+const multer = require('multer');
+const fs = require('fs');
 
 const User = require('../models/user.js');
+const Group = require('../models/group.js');
 
-router.post("/register", (req, res) => {
-  console.log(req.body);
-
-  var newUser = new User({
-    email: req.body.email,
-    password: req.body.password,
-    display_name: req.body.display_name,
-    created_at: Date.now()
-  });
-
-  console.log("PRE SAVE");
-
-  newUser.save(function(err, user) {
-    console.log("User created");
-    if (err) {
-      console.log(err);
-      return res.status(500).json({success: true, err: err.msg});
+var storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    console.log(req.body);
+    if (!fs.existsSync('./images')) {
+      console.log("Creating images folder.");
+      fs.mkdirSync('./images');
     }
 
-    const token = jwt.encode(user, config.secret);
-    return res.status(200).json({success: true, msg: "CREATED_NEW_USER", token: token});
+    var dir = "./images/" + res.locals.user.id;
+    if (!fs.existsSync(dir)) {
+      console.log("Creating dir:" + dir);
+      fs.mkdirSync(dir);
+    }
+
+    cb(null, dir);
+  },
+  filename: function(req, file, cb) {
+    cb(null, "profile.jpg");
+  }
+});
+
+var upload = multer({
+  storage: storage,
+  onError: function(err, next) {
+    console.log(err);
+    next(err);
+  }
+});
+
+var createUser = function(req, res, next) {
+  console.log(req.body);
+
+  Group.findOne({}, function(err, group) {
+    if (err) throw err;
+
+    var newUser = new User({
+      email: req.body.email,
+      password: req.body.password,
+      display_name: req.body.display_name,
+      groups: [group._id],
+      created_at: Date.now()
+    });
+
+    newUser.save(function(err, user) {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({success: true, err: err.msg});
+      }
+      console.log("User created");
+
+      const token = jwt.encode(user, config.secret);
+
+      res.locals.user = user;
+      res.locals.token = token;
+      //return res.status(200).json({success: true, msg: "CREATED_NEW_USER", token: token});
+    });
   });
+}
+
+router.post("/register", createUser, upload.single('image'), (req, res) => {
+  res.status(200).json({success: true, user: res.locals.user, token: res.locals.token});
 });
 
 router.post("/login", (req, res) => {
@@ -53,7 +95,7 @@ router.post("/login", (req, res) => {
   });
 });
 
-router.get('/logout', passport.authenticate('jwt', {session: false}), function(req, res) {
+router.get('/', passport.authenticate('jwt', {session: false}), function(req, res) {
   req.logout();
   res.json({success: true, msg: 'Sign out successful.'});
 });
