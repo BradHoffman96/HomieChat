0d3e7fa12439ef55b426ae66a71fe0922486c96f
Brad Hoffman <bradleydavidhoffman@gmail.com>
2019-07-20 00:58:35 -0400

    added GET /:id, /:id/image, /:id/members, and POST /:id routes

--- Server/routes/groups.js
+++ Server/routes/groups.js
@@ -75,7 +75,7 @@ router.post("/", passport.authenticate('jwt', {session:false}), function(req, re
   });
 });
 
-router.post("/:id", passport.authenticate('jwt', {session: false}), upload.single('image'), function(req, res) {
+router.post("/:id", passport.authenticate('jwt', {session: false}), function(req, res) {
   var groupId = req.params.id;
 
   Group.findById(groupId, function(err, group) {
@@ -83,8 +83,7 @@ router.post("/:id", passport.authenticate('jwt', {session: false}), upload.singl
 
 
     if (group) {
-
-      if (!group.members.includes(req.user.id)) {
+      if (!group.members.includes(req.user._id)) {
         return res.status(500).json({success: false, msg: "You are not authorized to edit this group."});
       }
 
@@ -94,15 +93,88 @@ router.post("/:id", passport.authenticate('jwt', {session: false}), upload.singl
       group.save(function(err, newGroup) {
         if (err) throw err;
 
-        if (newGroup) {
-          res.status(200).json({success: true, msg: "Group updated."});
-        } else {
-          res.status(400).json({success: false, msg: "An error occurred updating group."});
+        if (!fs.existsSync('./images')) {
+          console.log("Creating images folder.");
+          fs.mkdirSync('./images');
         }
+
+        var dir = "./images/" + newGroup._id;
+        if (!fs.existsSync(dir)) {
+          console.log("Creating dir:" + dir);
+          fs.mkdirSync(dir);
+        }
+
+        //upload image
+        fs.writeFile(dir + "/group.png", new Buffer(req.body.image, 'base64'), function(err) {
+          if (err) throw err;
+
+          console.log("File uploaded!")
+
+          if (newGroup) {
+            return res.status(200).json({success: true, msg: "Group successfully updated."});
+          } else {
+            return res.status(400).json({success: false, msg: "Something went wrong."});
+          }
+        });
       });
     } else {
       res.status(400).json({success: false, msg: "Could not find group."});
     }
+  });
+});
+
+router.get("/:id", passport.authenticate('jwt', {session: false}), function (req, res) {
+  var groupId = req.params.id;
+
+  Group.findById(groupId, function(err, group) {
+    if (err) throw err;
+
+    if (group) {
+      res.status(200).json({success: true, group: group});
+    } else {
+      res.status(400).json({success: false, msg: "Something went wrong getting group."});
+    }
+  });
+});
+
+router.get("/:id/image", passport.authenticate('jwt', {session: false}), function(req, res) {
+  var groupId = req.params.id;
+
+  var rootDir = path.join(__dirname, '../images', String(groupId), "group.png");
+
+  if (!fs.existsSync(rootDir)) {
+    console.log("Does not exist: " + rootDir);
+    res.status(400).json({success: false, msg:"Image cannot be found"});
+  } else {
+    res.sendFile(rootDir, {success: true, msg: "Found image for: " + req.user.email}, function(err) {
+      if (err) {
+        return res.status(500).json({success: false, msg: "Something went wrong while retrieiving image."});
+      } else {
+        console.log("Profile Image sent for: " + req.user.email);
+      }
+    });
+  }
+});
+
+router.get("/:id/members", passport.authenticate('jwt', {session: false}), function(req, res) {
+  var groupId = req.params.id;
+
+  Group.findById(groupId, function(err, group) {
+    if (err) throw err;
+
+    var users = [];
+    for (let userId in group.members) {
+      User.findById(userId, function(err, user) {
+        if (err) throw err;
+
+        if (user) {
+          user.password = undefined;
+          users.push(user);
+        }
+      });
+    }
+
+    res.status(200).json({success: true, users: users});
   });
 });
 
