165ac6542df80afdb3a23a77b0d8705e52c92f4e
Brad Hoffman <bradleydavidhoffman@gmail.com>
2019-08-07 09:03:11 +0200

    added some comments that I forgot to commit

--- homie_chat/lib/core/services/socket_service.dart
+++ homie_chat/lib/core/services/socket_service.dart
@@ -1,4 +1,7 @@
 
-class SocketService {
+class MessageService {
+
+  //TODO: Initialize the sockets, subscribe to necessary events, and connect to the socket
+  //TODO: Pipe the socket subscription callbacks to the Streams to update the 
 
 }
