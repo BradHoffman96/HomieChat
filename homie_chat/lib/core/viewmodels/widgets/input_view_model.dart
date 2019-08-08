d7f53dbb548a1828eba74ca379461b9a147bc7ca
Brad Hoffman <bradleydavidhoffman@gmail.com>
2019-08-08 22:48:45 +0200

    implemented InputView, but I've been pretty distracted, so I doubt any of this works. I am anticipating conflicts with the state in this home view. It just seems a little complicated with having services that both widgets need to subscribe to. I want to make sure I am not rebuilding, if I don't need to

---
+++ homie_chat/lib/core/viewmodels/widgets/input_view_model.dart
@@ -0,0 +1,30 @@
+
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:homie_chat/core/models/message.dart';
+import 'package:homie_chat/core/models/user.dart';
+import 'package:homie_chat/core/services/message_service.dart';
+import 'package:homie_chat/core/viewmodels/base_model.dart';
+
+class InputViewModel extends BaseModel {
+  MessageService _messageService;
+
+  InputViewModel({
+    MessageService messageService
+  }) : _messageService = messageService;
+
+  Future<bool> sendMessage(User user, String content) async {
+    setBusy(true);
+
+    var payload = {
+      "userId": user.id,
+      "content": content
+    };
+
+    var result = await _messageService.sendMessage(json.encode(payload));
+
+    return result;
+  }
+
+}
--- homie_chat/lib/ui/widgets/input_view.dart
+++ homie_chat/lib/ui/widgets/input_view.dart
@@ -1,10 +1,103 @@
 import 'package:flutter/material.dart';
+import 'package:homie_chat/core/viewmodels/widgets/input_view_model.dart';
+import 'package:homie_chat/ui/views/base_widget.dart';
+import 'package:provider/provider.dart';
 
-class InputView extends StatelessWidget {
+class InputView extends StatefulWidget {
+  @override
+  _InputViewState createState() => _InputViewState();
+}
+
+class _InputViewState extends State<InputView> {
+  final TextEditingController _textController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return BaseWidget<InputViewModel>(
+      model: InputViewModel(messageService: Provider.of(context)),
+      builder: (context, model, child) => Container(
+        padding: EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom),
+        decoration: BoxDecoration(color: Theme.of(context).cardColor),
+        child: _textComposer(context),
+      ),
     );
+  }
+
+  _handleSubmittedMessage() {
+    print(_textController.text);
+    _textController.clear();
+  }
+
+  Widget _textComposer(BuildContext context) {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+            Container(
+              //margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                icon: Icon(Icons.camera_alt),
+                onPressed: _showMediaSelectionSheet(context),
+              ),
+            ),
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                minLines: 1,
+                maxLines: 8,
+                decoration: InputDecoration.collapsed(hintText: "Message"),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                icon: Icon(Icons.send),
+                onPressed: () => _handleSubmittedMessage(),
+              ),
+            )
+          ],
+        )
+      ),
+    );
+  }
+
+  _showMediaSelectionSheet(BuildContext context) {
+    showModalBottomSheet(context: context, builder: (BuildContext context) {
+      return new Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          new ListTile(
+            leading: new Icon(Icons.camera_alt),
+            title: new Text('Camera'),
+            onTap: () async {
+              Navigator.pop(context);
+              print("Camera");
+              /*
+              var result = await getImageFromCamera();
+              if (result) {
+                _handleSubmittedMessage("");
+              }
+              */
+            }
+          ),
+          new ListTile(
+            leading: new Icon(Icons.photo_album),
+            title: new Text('Gallery'),
+            onTap: () async {
+              Navigator.pop(context);
+              print("Gallery");
+              /*
+              var result = await getImageFromGallery();
+              if (result) {
+                _handleSubmittedMessage("");
+              }
+              */
+            }
+          ),
+        ],
+      );
+    });
   }
 }
