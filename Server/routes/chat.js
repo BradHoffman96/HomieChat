7e715e1a7d90ffe89b93f9781e247c5eccac8244
Brad Hoffman <bradleydavidhoffman@gmail.com>
2019-08-10 21:40:08 +0200

    added an endpoint for pulling the 50 most recent messages

--- Server/routes/chat.js
+++ Server/routes/chat.js
@@ -2,10 +2,25 @@ const passport = require('passport');
 require('../config/passport')(passport);
 const router = require('express').Router();
 const jwt = require('jwt-simple');
+const Message = require('../models/message.js');
 
 
 router.post("/", passport.authenticate('jwt', {session:false}), function(req, res) {
   res.status(200).json({success: true, msg: "POST msg"});
+});
+
+router.get("/", passport.authenticate('jwt', {session:false}), function(req, res) {
+  Message.find().sort({_id:-1}).limit(50).exec(function(err, messages) {
+    if (err) throw err;
+
+    if (messages) {
+      console.log(messages);
+
+      res.status(200).json({success: true, messages: messages});
+    } else {
+      res.status(400).json({success: false, msg: "Could not get messages"});
+    }
+  });
 });
 
 module.exports = router;
