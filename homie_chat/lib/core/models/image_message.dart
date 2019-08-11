c116ab5992785892e43d4c233ea048f86adac37f
Brad Hoffman <bradleydavidhoffman@gmail.com>
2019-08-11 14:28:31 +0200

    added an ImageMessage model for easier understanding in Gallery code

---
+++ homie_chat/lib/core/models/image_message.dart
@@ -0,0 +1,26 @@
+//This is identical to a message, but it makes understanding a little more straightforward with the gallery
+import 'dart:convert';
+import 'dart:typed_data';
+
+class ImageMessage {
+  String id;
+  String senderId;
+  String timestamp;
+  Uint8List image;
+  //int likes;
+  //File image;
+
+  ImageMessage({this.senderId, this.id, /*this.likes,*/ this.timestamp});
+
+  ImageMessage.fromJson(Map<String, dynamic> json) {
+    senderId = json['sender'];
+    id = json['_id'];
+    timestamp = json['timestamp'];
+
+    //Might have to add a type variable for message to make this process a little easier
+    if (json['image'] != null) {
+      image = base64Decode(json['image']) ;
+    }
+  }
+
+}
