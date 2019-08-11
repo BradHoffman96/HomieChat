d81e92c3bba69bb30005925b897361eee38b7932
Brad Hoffman <bradleydavidhoffman@gmail.com>
2019-08-11 13:46:19 +0200

    wrapped model.busy in a Flexible so te InputView is always on the bottom of the screen

--- homie_chat/lib/ui/widgets/message_view.dart
+++ homie_chat/lib/ui/widgets/message_view.dart
@@ -24,53 +24,53 @@ class MessageView extends StatelessWidget {
         authenticationService: Provider.of(context),
         messageService: Provider.of(context)),
       onModelReady: (model) => model.connectToSocket(),
-      builder: (context, model, child) => model.busy
-        ? Column(
-          children: <Widget>[CircularProgressIndicator()],
-        ) : _messageView(context, model)
+      builder: (context, model, child) => Flexible(
+        child: model.busy
+          ? Column(
+            children: <Widget>[CircularProgressIndicator()],
+          ) : _messageView(context, model)
+      )
     );
   }
 
   Widget _messageView(BuildContext context, MessageViewModel model) {
     double c_width = MediaQuery.of(context).size.width * 0.8;
 
-    return Flexible(
-      child: ListView.builder(
-        padding: EdgeInsets.all(8.0),
-        reverse: true,
-        itemBuilder: (_, int index) {
-          Message message = model.messages[index];
-          User author = group.members[message.senderId];
+    return ListView.builder(
+      padding: EdgeInsets.all(8.0),
+      reverse: true,
+      itemBuilder: (_, int index) {
+        Message message = model.messages[index];
+        User author = group.members[message.senderId];
 
-          return Container(
-            margin: EdgeInsets.symmetric(vertical: 10.0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                //TODO: Going to have to pull this widget out when I implement actual profile pictures
-                Container(
-                  margin: EdgeInsets.only(right: 16.0),
-                  child: CircleAvatar(child: Text(author.displayName[0]))
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(author.displayName, style: Theme.of(context).textTheme.subhead),
-                    Container(
-                      margin: EdgeInsets.only(top: 5.0),
-                      width: c_width,
-                      child: (message.image != null) 
-                        ? Image.memory(message.image)
-                        : Text(message.text, textAlign: TextAlign.left,)
-                    )
-                  ],
-                )
-              ],
-            ),
-          );
-        },
-        itemCount: model.messages.length,
-      ),
+        return Container(
+          margin: EdgeInsets.symmetric(vertical: 10.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              //TODO: Going to have to pull this widget out when I implement actual profile pictures
+              Container(
+                margin: EdgeInsets.only(right: 16.0),
+                child: CircleAvatar(child: Text(author.displayName[0]))
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(author.displayName, style: Theme.of(context).textTheme.subhead),
+                  Container(
+                    margin: EdgeInsets.only(top: 5.0),
+                    width: c_width,
+                    child: (message.image != null) 
+                      ? Image.memory(message.image)
+                      : Text(message.text, textAlign: TextAlign.left,)
+                  )
+                ],
+              )
+            ],
+          ),
+        );
+      },
+      itemCount: model.messages.length,
     );
   }
 }
