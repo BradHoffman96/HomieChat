f94e7195c694aae3a2b16e35fdd3aa82ac810b50
Brad Hoffman <bradleydavidhoffman@gmail.com>
2019-08-18 13:32:30 +0200

    added video calling, but cannot do anything with it without having my own TURN server

--- homie_chat/lib/ui/views/video_call_view.dart
+++ homie_chat/lib/ui/views/video_call_view.dart
@@ -1,105 +1,223 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_webrtc/webrtc.dart';
+import 'package:homie_chat/core/services/signaling.dart';
+import 'dart:io';
 import 'dart:core';
+import 'package:flutter_webrtc/webrtc.dart';
 
-/**
- * getUserMedia sample
- */
 class VideoCallView extends StatefulWidget {
-  static String tag = 'get_usermedia_sample';
+  static String tag = 'call_sample';
+
+  VideoCallView({Key key}) : super(key: key);
 
   @override
   _VideoCallViewState createState() => new _VideoCallViewState();
 }
 
 class _VideoCallViewState extends State<VideoCallView> {
-  MediaStream _localStream;
-  final _localRenderer = new RTCVideoRenderer();
+  Signaling _signaling;
+  String _displayName =
+      Platform.localHostname + '(' + Platform.operatingSystem + ")";
+  List<dynamic> _peers;
+  var _selfId;
+  RTCVideoRenderer _localRenderer = new RTCVideoRenderer();
+  RTCVideoRenderer _remoteRenderer = new RTCVideoRenderer();
   bool _inCalling = false;
+  final String serverIP = "18.197.109.255:8086";
+
+  _VideoCallViewState({Key key});
 
   @override
   initState() {
     super.initState();
     initRenderers();
+    _connect();
+  }
+
+  initRenderers() async {
+    await _localRenderer.initialize();
+    await _remoteRenderer.initialize();
   }
 
   @override
   deactivate() {
     super.deactivate();
-    if (_inCalling) {
-      _hangUp();
+    if (_signaling != null) _signaling.close();
+    _localRenderer.dispose();
+    _remoteRenderer.dispose();
+  }
+
+  void _connect() async {
+    if (_signaling == null) {
+      _signaling = new Signaling(serverIP, _displayName)
+        ..connect();
+
+      _signaling.onStateChange = (SignalingState state) {
+        switch (state) {
+          case SignalingState.CallStateNew:
+            this.setState(() {
+              _inCalling = true;
+            });
+            break;
+          case SignalingState.CallStateBye:
+            this.setState(() {
+              _localRenderer.srcObject = null;
+              _remoteRenderer.srcObject = null;
+              _inCalling = false;
+            });
+            break;
+          case SignalingState.CallStateInvite:
+          case SignalingState.CallStateConnected:
+          case SignalingState.CallStateRinging:
+          case SignalingState.ConnectionClosed:
+          case SignalingState.ConnectionError:
+          case SignalingState.ConnectionOpen:
+            break;
+        }
+      };
+
+      _signaling.onPeersUpdate = ((event) {
+        this.setState(() {
+          _selfId = event['self'];
+          _peers = event['peers'];
+        });
+      });
+
+      _signaling.onLocalStream = ((stream) {
+        _localRenderer.srcObject = stream;
+      });
+
+      _signaling.onAddRemoteStream = ((stream) {
+        _remoteRenderer.srcObject = stream;
+      });
+
+      _signaling.onRemoveRemoteStream = ((stream) {
+        _remoteRenderer.srcObject = null;
+      });
     }
   }
 
-  initRenderers() async {
-    await _localRenderer.initialize();
+  _invitePeer(context, peerId, use_screen) async {
+    if (_signaling != null && peerId != _selfId) {
+      _signaling.invite(peerId, 'video', use_screen);
+    }
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  _makeCall() async {
-    final Map<String, dynamic> mediaConstraints = {
-      "audio": true,
-      "video": {
-        "mandatory": {
-          "minWidth":'640', // Provide your own width, height and frame rate here
-          "minHeight": '480',
-          "minFrameRate": '30',
-        },
-        "facingMode": "user",
-        "optional": [],
-      }
-    };
-
-    try {
-      var stream = await navigator.getUserMedia(mediaConstraints);
-      _localStream = stream;
-      _localRenderer.srcObject = _localStream;
-    } catch (e) {
-      print(e.toString());
+  _hangUp() {
+    if (_signaling != null) {
+      _signaling.bye();
     }
-    if (!mounted) return;
-
-    setState(() {
-      _inCalling = true;
-    });
   }
 
-  _hangUp() async {
-    try {
-      await _localStream.dispose();
-      _localRenderer.srcObject = null;
-    } catch (e) {
-      print(e.toString());
-    }
-    setState(() {
-      _inCalling = false;
-    });
+  _switchCamera() {
+    _signaling.switchCamera();
+  }
+
+  _muteMic() {
+
+  }
+
+  _buildRow(context, peer) {
+    var self = (peer['id'] == _selfId);
+    return ListBody(children: <Widget>[
+      ListTile(
+        title: Text(self
+            ? peer['name'] + '[Your self]'
+            : peer['name'] + '[' + peer['user_agent'] + ']'),
+        onTap: null,
+        trailing: new SizedBox(
+            width: 100.0,
+            child: new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  IconButton(
+                    icon: const Icon(Icons.videocam),
+                    onPressed: () => _invitePeer(context, peer['id'], false),
+                    tooltip: 'Video calling',
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.screen_share),
+                    onPressed: () => _invitePeer(context, peer['id'], true),
+                    tooltip: 'Screen sharing',
+                  )
+                ])),
+        subtitle: Text('id: ' + peer['id']),
+      ),
+      Divider()
+    ]);
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('GetUserMedia API Test'),
+        title: new Text('P2P Call Sample'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: null,
+            tooltip: 'setup',
+          ),
+        ],
       ),
-      body: new OrientationBuilder(
-        builder: (context, orientation) {
-          return new Center(
-            child: new Container(
-              margin: new EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-              child: RTCVideoView(_localRenderer),
-              decoration: new BoxDecoration(color: Colors.black54),
-            ),
-          );
-        },
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _inCalling ? _hangUp : _makeCall,
-        tooltip: _inCalling ? 'Hangup' : 'Call',
-        child: new Icon(_inCalling ? Icons.call_end : Icons.phone),
-      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: _inCalling
+          ? new SizedBox(
+            width: 200.0,
+            child: new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  FloatingActionButton(
+                    child: const Icon(Icons.switch_camera),
+                    onPressed: _switchCamera,
+                  ),
+                  FloatingActionButton(
+                    onPressed: _hangUp,
+                    tooltip: 'Hangup',
+                    child: new Icon(Icons.call_end),
+                    backgroundColor: Colors.pink,
+                  ),
+                  FloatingActionButton(
+                    child: const Icon(Icons.mic_off),
+                    onPressed: _muteMic,
+                  )
+                ])) : null,
+      body: _inCalling
+          ? OrientationBuilder(builder: (context, orientation) {
+              return new Container(
+                child: new Stack(children: <Widget>[
+                  new Positioned(
+                      left: 0.0,
+                      right: 0.0,
+                      top: 0.0,
+                      bottom: 0.0,
+                      child: new Container(
+                        margin: new EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height,
+                        child: new RTCVideoView(_remoteRenderer),
+                        decoration: new BoxDecoration(color: Colors.black54),
+                      )),
+                  new Positioned(
+                    left: 20.0,
+                    top: 20.0,
+                    child: new Container(
+                      width: orientation == Orientation.portrait ? 90.0 : 120.0,
+                      height:
+                          orientation == Orientation.portrait ? 120.0 : 90.0,
+                      child: new RTCVideoView(_localRenderer),
+                      decoration: new BoxDecoration(color: Colors.black54),
+                    ),
+                  ),
+                ]),
+              );
+            })
+          : new ListView.builder(
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(0.0),
+              itemCount: (_peers != null ? _peers.length : 0),
+              itemBuilder: (context, i) {
+                return _buildRow(context, _peers[i]);
+              }),
     );
   }
 }
